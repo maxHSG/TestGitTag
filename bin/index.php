c9f6b094d81767a8c875e2ee4eca62766c34f3f3
Max <max.h.siemermann@gmail.com>
2020-12-01 14:41:06 -0300

    teste

--- bin/index.php
+++ bin/index.php
@@ -1,5 +1,20 @@
 <?php
 
+$branch_name = exec("git branch --show current");
 
+if ($branch_name === "master") {
+    try {
+        $tag =  exec("git describe --tags --abbrev=0 --exact-match");
+        
+        $composer_path = "./composer.json";
+        
+        $composer_json = json_decode(file_get_contents($composer_path));
+        
+        $composer_json->version = $tag;
+        
+        file_put_contents($composer_path, json_encode($composer_json, JSON_UNESCAPED_SLASHES));
 
-echo exec("git tag");
+        exec("git add composer.json");
+    } catch (\Throwable $th) {
+    }
+}

5b49631e2ac93673a2ac6887b155490258fe163c
max henrique <max.h.siemermann@gmail.com>
2023-06-27 22:25:54 -0300

    sIM

--- bin/index.php
+++ bin/index.php
@@ -5,6 +5,8 @@ $branch_name = exec("git branch --show current");
 $push = isset($argv[4]) ? $argv[4] : null;
 
 
+// Teste2
+
 if ($branch_name === "master" && !$push) {
     $tag =  exec("git describe --tags");
 
@@ -13,15 +15,15 @@ if ($branch_name === "master" && !$push) {
     $commit = "Update version to {$tag}";
 
     $composer_path = "./composer.json";
-        
+
     $composer_json = json_decode(file_get_contents($composer_path));
 
     if ($composer_json->version === $tag) {
         return;
     }
-        
+
     $composer_json->version = $tag;
-        
+
     file_put_contents($composer_path, json_encode($composer_json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
 
     exec("git add composer.json");
